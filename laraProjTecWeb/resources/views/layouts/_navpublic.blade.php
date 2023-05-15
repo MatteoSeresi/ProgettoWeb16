a20c8b9e34a8c39df5466148bc62cf307737ae81
Mattia <s1093418@studenti.univpm.it>
2023-05-15 16:15:12 +0200

    navbar

---
+++ laraProjTecWeb/resources/views/layouts/_navpublic.blade.php
@@ -0,0 +1,73 @@
+<div class="container-fluid container-lg my-3 mb-0 pb-3 border-bottom border-2 border-black">
+        <div class="row align-items-center justify-content-center">
+            <div class="col d-none d-lg-block">
+            </div>
+            <div class="col-12 col-lg-6 text-center">
+                <a href="home.html"><img src="../img/provaimg.jpg" style="width:500px; margin: auto;"></a>
+                <h1>TITOLO SITO</h1>
+            </div>
+            <div class="col d-none d-lg-block "> </div>
+        </div>
+</div>
+        <nav class="sticky-top bg-old ">
+            <div class="container-fluid container-lg p-0 border-bottom border-2 border-black">
+                <table class="table text-nowrap text-center m-0 fw-black" style="min-width: auto; width: auto; margin: auto!important;">
+                    <tbody>
+                        <tr class="align-middle">
+                            <td class="p-0">
+                                <a href="{{ route('home') }}" class="p-3 lh-1 d-block spa text-black text-decoration-none active">
+                                    HOME
+                                </a>
+                            </td>
+                            <td class="px-0">
+                                <div class="vr"></div>
+                            </td>
+                            <td class="p-0">
+                                <a href="{{ route('aziende') }}" class="p-3 lh-1 d-block spa type text-black text-decoration-none">
+                                    AZIENDE
+                                </a>
+                            </td>
+                            <td class="px-0">
+                                <div class="vr"></div>
+                            </td>
+                            <td class="p-0">
+                                <a href="{{ route('catalog1') }}" class="p-3 lh-1 d-block spa type text-black text-decoration-none ">
+                                    CATALOGO
+                                </a>
+                            </td>
+                            <td class="px-0">
+                                <div class="vr"></div>
+                            </td>
+                            <td class="p-0">
+                                <a href="{{ route('faq') }}" class="p-3 lh-1 d-block spa type text-black text-decoration-none ">
+                                    FAQ
+                                </a>
+                            </td>
+                            <td class="px-0">
+                                <div class="vr"></div>
+                            </td>
+                            <td class="p-0">
+                                <a href="{{ route('where') }}" class="p-3 lh-1 d-block spa type text-black text-decoration-none ">
+                                    CONTATTACI
+                            </td>
+                            <td class="px-0">
+                                <div class="vr"></div>
+                            </td>
+                            <td class="p-0">
+                                <a href="{{ route('where') }}" class="p-3 lh-1 d-block spa type text-black text-decoration-none ">
+                                    ACCEDI
+                                </a>
+                            </td>
+                            <td class="px-0">
+                                <div class="vr"></div>
+                            </td>
+                                <td class="p-0">
+                                    <a href="{{ route('where') }}" class="p-3 lh-1 d-block spa text-black text-decoration-none ">
+                                        REGISTRATI
+                                    </a>
+                                </td>                                                             
+                        </tr>
+                    </tbody>
+                </table>         
+            </div>
+        </nav>
