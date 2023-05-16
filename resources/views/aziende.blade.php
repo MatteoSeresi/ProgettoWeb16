e6ca1fa3d644155d3c49a6d6419fff4d1dcaf32a
Mattia <s1093418@studenti.univpm.it>
2023-05-16 16:48:22 +0200

    azienda completo

--- resources/views/aziende.blade.php
+++ resources/views/aziende.blade.php
@@ -5,14 +5,15 @@
 @section('content')
 <section id="azienda">
     <div id="high_c">
+        <img src="../../public/images/lebron.jpg" alt="immagine azienda">
         <h1>NOME AZIENDA</h1>
     </div>
-    <div id="mid_c">
-        <img src="lebron.jpg" alt="immagine azienda"> 
-    </div>
-    <center id="low_c">
-        <div id="slot_img">
-            <img src="lebron.jpg" alt="posizione" onclick="window.open('https://it.wikipedia.org/wiki/LeBron_James')">
+    <div id="low_c">
+        <div id="slot">
+            <iframe src="https://maps.google.com/maps?width=200&amp;height=200&amp;hl=en&amp;
+                q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"  width="200" height="200"
+                frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a>
+            </iframe>
         </div>
 
         <div id="slot">
@@ -22,7 +23,33 @@
         <div id="slot">
             <p>Qui ci sono i contatti dell'azienda</p>
         </div>
-    </center>
+    </div>
+    <div id="end_c">
+        <a href="" >Vai alle offerte</a>
+    </div>
+</section>
+
+<section id="azienda">
+    <div id="high_c">
+        <img src="../../public/images/lebron.jpg" alt="immagine azienda">
+        <h1>NOME AZIENDA</h1>
+    </div>
+    <div id="low_c">
+        <div id="slot">
+            <iframe src="https://maps.google.com/maps?width=200&amp;height=200&amp;hl=en&amp;
+                q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"  width="200" height="200"
+                frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a>
+            </iframe>
+        </div>
+
+        <div id="slot">
+            <p>Questa è la descrizione dell'azienda</p>
+        </div>
+
+        <div id="slot">
+            <p>Qui ci sono i contatti dell'azienda</p>
+        </div>
+    </div>
     <div id="end_c">
         <a href="" >Vai alle offerte</a>
     </div>
