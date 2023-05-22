98dbdefab0e6c15537466b0a9193ef9d44bc7216
MatteoSeresi <s1100028@studenti.univpm.it>
2023-05-22 11:47:02 +0200

    Alcune modifche

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,8 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 class UserController extends Controller {
+
+    protected $_utenteModel;
+
+    public function __construct() {
+        $this->_utenteModel = new User;
+        $this->middleware('can:isUser');
+    }
 
     public function userarea() {
         return view('user');
