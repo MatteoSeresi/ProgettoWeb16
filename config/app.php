f20366451e34c80f1f7874762b0ec69b2da3c67e
Diego7imi <diegosettimi01@gmail.com>
2023-05-22 15:47:49 +0200

    risolto problema form

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
 
         
 
@@ -211,7 +212,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        
+        'Form' => Collective\Html\FormFacade::class,
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
