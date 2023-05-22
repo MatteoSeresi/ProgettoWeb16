ea7b74eb1b9ec5bcea657f31563b143d7ee3ec21
Diego7imi <diegosettimi01@gmail.com>
2023-05-22 16:41:26 +0200

    Revert "Alcune modifche"
    This reverts commit 98dbdefab0e6c15537466b0a9193ef9d44bc7216.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,16 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
 
 class UserController extends Controller {
-
-    protected $_utenteModel;
-
-    public function __construct() {
-        $this->_utenteModel = new User;
-        $this->middleware('can:isUser');
-    }
 
     public function userarea() {
         return view('user');
